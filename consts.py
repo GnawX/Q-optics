f0d5bbf692977faf72f1d384057155bab53d6924
Xiaoming Wang <40604687+GnawX@users.noreply.github.com>
2026-01-08 14:02:55 -0500

    Replace numpy with torch for constants and types

--- consts.py
+++ consts.py
@@ -1,13 +1,11 @@
-import numpy as np
-import ctypes
+import torch
 
-ftype = np.float32
-ctype = np.complex64
-chalf = (ctypes.c_float * 2)(0.5, 0)
-zhalf = (ctypes.c_double * 2)(0.5, 0)
+ftype = torch.float32 
+ctype = torch.complex64
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-PI      = ftype(np.pi)
-C_LIGHT = ftype(137.035999084)  # speed of light
-AU2EV   = ftype(27.211386245988)  # Hartree to eV
-AU2A    = ftype(0.52917721092)     # Bohr to Angstrom
 
+PI      = torch.tensor(torch.pi, dtype=ftype)
+C_LIGHT = torch.tensor(137.035999084, dtype=ftype)  # speed of light
+AU2EV   = torch.tensor(27.211386245988, dtype=ftype)  # Hartree to eV
+AU2A    = torch.tensor(0.52917721092, dtype=ftype)     # Bohr to Angstrom
